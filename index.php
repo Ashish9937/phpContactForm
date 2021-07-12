56ebea0c5be729ae455d864b665f89332f44d70a
Ashish9937 <69027728+Ashish9937@users.noreply.github.com>
2021-07-12 21:47:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,21 +5,16 @@
   $website = htmlspecialchars($_POST['website']);
   $message = htmlspecialchars($_POST['message']);
 
-  if(!empty($email) && !empty($message)){
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "ashishagrahari98@gmail.com"; //enter that email address where you want to receive all messages
+      $receiver = "ashishagrahari98@gmail.com";
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
-      if(mail($receiver, $subject, $body, $sender)){
+      if(mail($receiver, $subject, $body, $sender))
+      {
          echo "Your message has been sent";
-      }else{
+      }
+      else
+      {
          echo "Sorry, failed to send your message!";
       }
-    }else{
-      echo "Enter a valid email address!";
-    }
-  }else{
-    echo "Email and message field is required!";
-  }
 ?>
